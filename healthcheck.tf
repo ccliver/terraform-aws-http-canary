d7c0dde0e66f9d1649c40932be435fd5fc70acf6
Carl Cliver <carl.cliver@Carls-MacBook-Pro.local>
2018-09-26 16:27:52 -0400

    More descriptive role name.

--- healthcheck.tf
+++ healthcheck.tf
@@ -44,7 +44,7 @@ resource "aws_cloudwatch_metric_alarm" "healthcheck" {
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
+  name = "http-healthcheck-${var.app_name}-${var.region}"
 
   assume_role_policy = <<EOF
 {
