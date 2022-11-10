539d892724da69ff1fbbda592c3aef99cfa4ef88
Carl Cliver <ccliver@gmail.com>
2022-11-09 21:08:51 -0500

    chore: cleaning up

--- http_check/http_check/http_check.py
+++ http_check/http_check/http_check.py
@@ -1,15 +1,10 @@
 """Lambda that performs an HTTP check on an endpoint"""
 
-
-#
-# TODO: Rebuild the module with poetry
-#
-
 import os
 from datetime import datetime
 
-import requests
 import boto3
+import requests
 
 
 def check_endpoint(endpoint: str) -> str:
@@ -59,74 +54,22 @@ def handler(event, context):
         context: Provides methods and properties that provide information about the invocation, function, and runtime environment.
     """
 
-    region = os.environ['AWS_REGION']
+    region = os.environ["AWS_REGION"]
     health_check_endpoint = os.environ["HEALTH_CHECK_ENDPOINT"]
     metric_name = os.environ["METRIC_NAME"]
-    acceptable_return_codes = os.environ.get("ACCEPTABLE_RETURN_CODES", "200")
+    acceptable_return_codes = os.environ.get("ACCEPTABLE_RETURN_CODES")
 
     http_status_code = check_endpoint(health_check_endpoint)
     print(f"HTTP status code: {http_status_code}")
     if http_status_code not in acceptable_return_codes:
         put_metric_data(metric_name, 1, region)
+        # TODO: send message to SNS with payload on failure
     else:
         put_metric_data(metric_name, 0, region)
-    # TODO: send message to SNS with payload on failure
 
     response = put_metric_data(metric_name, http_status_code, region)
     print(f"PutMetricData Response: {response}")
 
 
-
 if __name__ == "__main__":
     handler("", "")
-
-
-# package main
-#
-# import (
-# 	"github.com/aws/aws-lambda-go/lambda"
-# 	"github.com/aws/aws-sdk-go/aws"
-# 	"github.com/aws/aws-sdk-go/aws/session"
-# 	"github.com/aws/aws-sdk-go/service/cloudwatch"
-# 	"net/http"
-# 	"os"
-# 	"time"
-# )
-#
-# func checkEndpoint(endpoint string) int {
-# 	resp, err := http.Get(endpoint)
-# 	if err != nil {
-# 		return 503
-# 	}
-#
-# 	return resp.StatusCode
-# }
-#
-# func healthcheck() {
-# 	endpoint := os.Getenv("HTTP_HEALTHCHECK_ENDPOINT")
-# 	metricName := "http-healthcheck-" + os.Getenv("APP_NAME") + "-" + os.Getenv("AWS_REGION")
-# 	timestamp := time.Now()
-# 	metricValue := float64(checkEndpoint(endpoint))
-#
-# 	data := cloudwatch.MetricDatum{
-# 		MetricName: &metricName,
-# 		Timestamp:  &timestamp,
-# 		Value:      &metricValue,
-# 	}
-#
-# 	metricDataList := []*cloudwatch.MetricDatum{&data}
-# 	metricDataInput := cloudwatch.PutMetricDataInput{
-# 		MetricData: metricDataList,
-# 		Namespace:  &metricName,
-# 	}
-#
-# 	sess := session.Must(session.NewSession(&aws.Config{
-# 		Region: aws.String(os.Getenv("AWS_REGION")),
-# 	}))
-# 	svc := cloudwatch.New(sess)
-# 	svc.PutMetricData(&metricDataInput)
-# }
-#
-# func main() {
-# 	lambda.Start(healthcheck)
-# }
